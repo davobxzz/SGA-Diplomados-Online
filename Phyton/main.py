2371ddf4c0e0d594c7862d218b5c56be3022373b
davobxzz <davobxzz@gmail.com>
2026-09-18 21:48:13 -0400

    Agregando interfaz del menú y programando opciones

---
+++ Phyton/main.py
@@ -0,0 +1,17 @@
+while True:
+    print("SGA Diplomados Online")
+    print("1. Registrar Alumno")
+    print("2. Registrar Profesor")
+    print("3. Registrar Notas")
+    print("4. Deshacer ultimo registro de notas")
+    print("5. Generar cola de certificados")
+    print("6. Mostrar reporte general")
+    print("7. Salir")
+
+    opcion = input("Seleccione una opcion del 1 al 7: ")
+
+    if opcion == "7":
+        print("Cerrando SGA Diplomados Online")
+        break 
+    
+
