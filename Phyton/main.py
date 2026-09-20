ba4a53bd64569059b2a0183bf00b26cee8bd0433
davobxzz <davobxzz@gmail.com>
2026-09-19 20:52:15 -0400

    Programando opcion 1

--- Phyton/main.py
+++ Phyton/main.py
@@ -1,3 +1,24 @@
+def registrar_alumno():
+    print("/n Registro de Alumno")
+    cedula = input("Ingrese la cedula:")
+    nombre = input("Ingrese el nombre:")
+    correo = input("Ingrese el correo:")
+    programa = input("Ingrese el programa:")
+
+    nota1 = 0
+    nota2 = 0
+    nota3 = 0
+
+    with open("alumnos.txt","a",
+              encoding="utf-8") as archivo:
+                        archivo.write(f"{cedula},{nombre},{correo},{programa},{nota1},{nota2},{nota3}\n")
+
+                            
+    print("Alumno registrado con exito")
+
+
+
+
 while True:
     print("SGA Diplomados Online")
     print("1. Registrar Alumno")
@@ -10,8 +31,11 @@ while True:
 
     opcion = input("Seleccione una opcion del 1 al 7: ")
 
+
+    if opcion == "1":
+            registrar_alumno()
+
+
     if opcion == "7":
         print("Cerrando SGA Diplomados Online")
         break 
-    
-
